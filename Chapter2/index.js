dc0a994609f20ce8848d04d555703442d80b213c
unknown <PHM1605@gmail.com>
2024-02-22 17:24:10 +0700

    updates

--- Chapter2/index.js
+++ Chapter2/index.js
@@ -44,8 +44,27 @@ function createCube(gl) {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
   
   // Color data
-  
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  var faceColors = [
+    [1.0, 0.0, 0.0, 1.0], // Front face
+    [0.0, 1.0, 0.0, 1.0], // Back face
+    [0.0, 0.0, 1.0, 1.0], // Top face
+    [1.0, 1.0, 0.0, 1.0], // Bottom face
+    [1.0, 0.0, 1.0, 1.0], // Right face
+    [0.0, 1.0, 1.0, 1.0] // Left face]
+  ];
+  // vertex color: [[color point0 face0],[color point1 face0],[]..., [color point3 face 5]]
+  for (var i in faceColors) {
+    var color = faceColors[i];
+    for (var j=0; j<4; j++) {
+      vertexColors = vertexColors.concat(color);
+    }
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
 
+  // Index data, defines the triangles to be drawn
+  
 }
 
 
